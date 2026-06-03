783bdbd1454201d9232d4db44a52adb8aa0b3d9e
Jesus Ocanto <yisusocanto1984@gmail.com>
2026-06-03 15:23:48 -0400

    Fixed TypeScript error in tokenOptional

--- server/src/middleware/tokenOptional.ts
+++ server/src/middleware/tokenOptional.ts
@@ -2,19 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import { auth } from "../lib/auth.js";
 
 export const tokenOptional = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
+	req: Request,
+	res: Response,
+	next: NextFunction,
 ) => {
-  const session = await auth.api.getSession({
-    headers: new Headers(req.headers as any),
-  });
+	const session = await auth.api.getSession({
+		headers: new Headers(req.headers as any),
+	});
 
-  if (session) {
-    req.user = {
-      _id: session.user.id,
-    };
-  }
+	if (session) {
+		req.user = { _id: session.user.id, email: session.user.email };
+	}
 
-  next();
+	next();
 };
